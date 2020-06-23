6e5fcbdd3137459f99e40711a464a00ffa81e4ba
Julien Chiquet <julien.chiquet@gmail.com>
2020-06-23 10:39:23 +0200

    updating documentation

--- R/aricode-package.R
+++ R/aricode-package.R
@@ -2,12 +2,15 @@
 #'
 #' A package for efficient computations of standard clustering comparison measures. Available measures are described in the paper of Vinh et al, JMLR, 2009 (see reference below).
 #'
-#' Traditional implementations (e.g., function \code{adjustedRandIndex} of package \code{mclust}) are in Omega(n + u v) where n is the size of the vectors the classifications of which are to be compared, u and v are the respective number of classes in each vectors. Here, the implementation is in Theta(n), plus the gain of speed due to the C code.
+#' Traditional implementations (e.g., function \code{adjustedRandIndex} of package \code{mclust}) are in Omega(n + u v) where n is the size of the vectors the classifications of which are to be compared, u and v are the respective number of classes in each vectors. Here, the implementation is in Theta(n), plus the gain of speed due to the C++ code.
 #'
 #' @section Functions in aricode:
 #' The functions included in aricode are:
 #' \itemize{
 #'  \item{ARI:}{ computes the adjusted rand index}
+#'  \item{Chi2:}{ computes the Chi-square statistics}
+#'  \item{MARI:}{ computes the modified adjusted rand index (Sundqvist et al, in preparation)}
+#'  \item{MARIraw:}{ computes the multinomial adjusted rand index}
 #'  \item{RI:}{ computes the rand index}
 #'  \item{NVI:}{ computes the normalized variation information}
 #'  \item{NID:}{ computes the normalized information distance}
@@ -16,8 +19,9 @@
 #'  \item{entropy:}{ computes the conditional and joint entropies}
 #'  \item{clustComp:}{ computes all clustering comparison measures at once}
 #' }
-#' @author Julien Chiquet \email{julien.chiquet@@gmail.com}
-#' @author Guillem Rigaill \email{guillem.rigaill@@evry.inra.fr}
+#' @author Julien Chiquet \email{julien.chiquet@@inrae.com}
+#' @author Guillem Rigaill \email{guillem.rigaill@@inrae.fr}
+#' @author Martina Sundqvist \email{martina.sundqvist@@agroparistech.fr}
 #' @references Nguyen Xuan Vinh, Julien Epps, and James Bailey. "Information theoretic measures for clusterings comparison: Variants, properties, normalization and correction for chance." Journal of Machine Learning Research 11.Oct (2010): 2837-2854. as described in Vinh et al (2009)
 #' @seealso \code{\link{ARI}}, \code{\link{RI}}, \code{\link{NID}}, \code{\link{NVI}}, \code{\link{AMI}}, \code{\link{NMI}}, \code{\link{entropy}}, \code{\link{clustComp}}
 #' @docType package
